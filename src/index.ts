15c1e65295360bdbfbb269736b084adfec58c228
Tyler Long <tyler4long@gmail.com>
2021-01-12 19:18:28 -0800

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import RingCentral from '@rc-ex/core';
+import waitFor from 'wait-for-async';
 
 const rc = new RingCentral({
   clientId: process.env.RINGCENTRAL_CLIENT_ID,
@@ -12,6 +13,24 @@ const rc = new RingCentral({
     extension: process.env.RINGCENTRAL_EXTENSION,
     password: process.env.RINGCENTRAL_PASSWORD!,
   });
-  console.log(rc.token?.access_token);
+
+  setInterval(async () => {
+    const activeCalls = await rc
+      .restapi()
+      .account()
+      .extension()
+      .activeCalls()
+      .get({view: 'Detailed', direction: ['Inbound']});
+    if (activeCalls.records!.length > 0) {
+      console.log(JSON.stringify(activeCalls.records![0], null, 2));
+    } else {
+      console.log('No records');
+    }
+    console.log(new Date());
+  }, 5000);
+
+  await waitFor({
+    interval: 999999999,
+  });
   await rc.revoke();
 })();
